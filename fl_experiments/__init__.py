bcf7e598d5615f7b2b4d21ac434da2fbc83bc08d
Christopher Briggs <confusedmatrix@gmail.com>
2020-05-27 12:36:09 +0100

    Moved Dataset file up a level

--- fl_experiments/__init__.py
+++ fl_experiments/__init__.py
@@ -1,3 +1,3 @@
-from datasets.Dataset import Dataset, FederatedDataset
+from Dataset import Dataset, FederatedDataset
 from Experiment import Experiment, FCFLExperiment
 from ExperimentSettings import SETTINGS_DEFINITIONS, ExperimentSettings
