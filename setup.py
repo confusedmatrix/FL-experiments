35ce9a5ac62dfcb1065d2afd152439cd5441eab0
Christopher Briggs <confusedmatrix@gmail.com>
2020-05-27 12:49:04 +0100

    Updated setup.py to load deps

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+
+def get_requirements(req_file):
+    """Read requirements file and return packages and git repos separately"""
+    requirements = []
+    dependency_links = []
+    lines = open(req_file).read().split("\n")
+    for line in lines:
+        if line.startswith("git+"):
+            dependency_links.append(line)
+        else:
+            requirements.append(line)
+    return requirements, dependency_links
+
+core_reqs, core_dependency_links = get_requirements("requirements.txt")
 
 setup(
     name='fl_experiments',
@@ -8,6 +23,8 @@ setup(
     author='confusedmatrix',
     author_email='confusedmatrix@gmail.com',
     license='MIT',
+    install_requires=core_reqs,
+    dependency_links=core_dependency_links,
     packages=['fl_experiments'],
     zip_safe=False
 )
